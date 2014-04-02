491a43213b024af5f4ef5a1acab0fd370768d028
Higorhicaro <higao_hh@hotmail.com>
2014-04-02 14:05:30 -0300

    User and Role tables created

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -113,3 +113,19 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.test.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.test.UserRole'
+grails.plugin.springsecurity.authority.className = 'com.test.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+
